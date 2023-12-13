e70dc72eaaac9ef9974564b4b96aa0dffbf19aa0
Karolkaz01 <kar.kaz_2001@wp.pl>
2023-12-13 13:11:54 +0100

    test env

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-// export const API_URL = (import.meta.env.VITE_API_URL || '') as string
-// export const API_KEY = (import.meta.env.VITE_API_KEY || '') as string
+export const API_URL = (import.meta.env.VITE_API_URL || '') as string
+export const API_KEY = (import.meta.env.VITE_API_KEY || '') as string
 
-export const API_URL = 'https://localhost:8080'
-export const API_KEY = 'kopytko'
+// export const API_URL = 'https://fancytodolist.azurewebsites.net'
+// export const API_KEY = '3a8e9a86-f0c7-4f22-86b4-ce71e0c9c3bc'
